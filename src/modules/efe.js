21944972892d37a354def1308c41cada0765e3c7
efe <jimmyfallon717@gmail.com>
2019-08-19 18:43:52 +0300

    added some beauty

--- src/modules/efe.js
+++ src/modules/efe.js
@@ -1,18 +1,52 @@
+const fetch = require("node-fetch");
+const reddit_link1 = 'https://www.reddit.com/r/Sexy/random.json';
+// const reddit_link2 = 'https://www.reddit.com/r/nudes/random.json';
+const reddit_link3 = 'https://www.reddit.com/r/BellaThorne/random.json';
+
 export default (bot) => {
 
-    bot.on('text', (ctx) => {
-       let messages = {
-           "ağla": "ÜHÜHÜHÜHÜHÜHÜHÜHÜHÜHÜHÜ",
-           "hapşu": "ÇOK YAŞA",
-           "delir": "OOOOWOAOWPOFQMPOFMEKPFMWOMDFEOIĞMDJWDNIEFN",
-           "": ""
+    bot.hears('bella', (ctx) => {
 
-       }
-        if (ctx.message.text.toLowerCase() in messages){
+        fetch(reddit_link3)
+            .then(response => response.json())
+            .then(data => ctx.replyWithPhoto(data[0]['data']['children'][0]['data']['url']))
+
+    })
+
+    //TODO: WORKING ON THIS
+    /*
+       bot.hears('nude', (ctx) => {
+   
+           fetch(reddit_link2)
+               .then(response => response.json())
+               .then(data => ctx.replyWithPhoto(data[0]['data']['children'][0]['data']['url']))
+   
+       })
+   */
+    bot.hears('girl', (ctx) => {
+
+        fetch(reddit_link1)
+            .then(response => response.json())
+            .then(data => ctx.replyWithPhoto(data[0]['data']['children'][0]['data']['url']))
+
+    })
+
+
+    bot.on('text', (ctx) => {
+        let messages = {
+            "ağla": "ÜHÜHÜHÜHÜHÜHÜHÜHÜHÜHÜHÜ",
+            "hapşu": "ÇOK YAŞA",
+            "delir": "OOOOWOAOWPOFQMPOFMEKPFMWOMDFEOIĞMDJWDNIEFN",
+            "sa": "as",
+            "merhaba": "merhaba",
+
+
+        }
+        if (ctx.message.text.toLowerCase() in messages) {
             ctx.reply(messages[ctx.message.text.toLowerCase()])
-       }else{
-           ctx.reply("EFENDIMMM")
-       }
+        } else {
+            ctx.reply("EFENDIMMM")
+        }
     })
 
 
